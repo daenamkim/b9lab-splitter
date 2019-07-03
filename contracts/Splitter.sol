8409d6efa4a54ad8795aa2ab20290e6157bfdacd
Daenam Kim <daenam.kim@gmail.com>
2019-07-04 07:05:02 +0900

    Contribute ether to others

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -10,10 +10,9 @@ contract Splitter {
   User[] users;
   mapping(address => bool) accounts;
 
-  event userAdded(User user);
-
   function addUser(bytes8 name, address addr) public {
     require(!accounts[addr], 'A given address is already registerd');
+    // TODO: limit the number of users?
 
     User memory newUser = User({
       name: name,
@@ -21,16 +20,29 @@ contract Splitter {
       balance: 0
     });
     users.push(newUser);
-    emit userAdded(newUser);
   }
 
-  event sendEtherHandle();
+  function contributeEther() public payable {
+    require(accounts[msg.sender], 'A given address is not registered');
+    require(users.length > 2, 'Please wait until at least 3 users are registered');
+    require(msg.value > 0, 'A given value should be bigger thatn 0 (wei)');
+    // TODO: handle float?
 
-  function sendEther() public payable {
+    uint splitEther = msg.value / (users.length - 1);
 
+    for (uint i = 0; i < users.length; i++) {
+      User storage user = users[i];
+      if (msg.sender != user.addr) {
+        user.balance += splitEther;
+      }
+    }
   }
 
   function getAllUsers() public view returns (User[] memory) {
     return users;
+  }
+
+  function getBalance() public view returns (uint) {
+    return address(this).balance;
   }
 }
