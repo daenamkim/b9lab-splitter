e5b6e423c772e6f52e5a9cb50d32df82cbb7643f
Daenam Kim <daenam.kim@gmail.com>
2019-07-07 08:36:32 +0900

    Make ganache accounts deterministic

--- ethereum/start-ganache.sh
+++ ethereum/start-ganache.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ./stop-ganache.sh
-yarn ganache-cli --accounts=10 --host=127.0.0.1 --quiet -u 0 -u 1 -u 2 -u 3 &
+yarn ganache-cli --accounts=10 --host=127.0.0.1 --quiet --deterministic &
