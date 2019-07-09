62f126c94caf8b0906447a42c38401c20a7e2e30
Daenam Kim <daenam.kim@gmail.com>
2019-07-10 08:27:13 +0900

    Fix event params

--- ethereum/contracts/Splitter.sol
+++ ethereum/contracts/Splitter.sol
@@ -8,18 +8,18 @@ contract Splitter {
     owner = msg.sender;
   }
 
-  event Transfer(address from, bool status, uint balance1, uint balance2);
+  event Transfer(address from, address receiver1, address receiver2);
 
   function sendEther(address payable receiver1, address payable receiver2) public payable {
     // One transfer call needs 2300 gas
     require(msg.sender.balance + 4600 > msg.value, 'A sender should have enough balance');
     require(msg.value > 0, 'A given value should be bigger than 0');
-    require(msg.sender != receivers[0] && msg.sender != receivers[1], 'A sender should not be one of receivers');
+    require(msg.sender != receiver1 && msg.sender != receiver2, 'A sender should not be one of receivers');
 
     uint value = msg.value / 2;
-    receiver1.transfer(value)
-    receiver2.transfer(value)
+    receiver1.transfer(value);
+    receiver2.transfer(value);
 
-    emit Transfer(msg.sender, true, receivers[0].balance, receivers[1].balance);
+    emit Transfer(msg.sender, receiver1, receiver2);
   }
 }
