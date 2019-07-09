e6e48af8e440011255bec0f56899e80d5f5365dc
Daenam Kim <daenam.kim@gmail.com>
2019-07-10 07:50:56 +0900

    Set fixed the number of receivers exactly

--- ethereum/contracts/Splitter.sol
+++ ethereum/contracts/Splitter.sol
@@ -10,17 +10,16 @@ contract Splitter {
 
   event Transfer(address from, bool status, uint balance1, uint balance2);
 
-  function sendEther(address[] memory receivers) public payable {
+  function sendEther(address payable receiver1, address payable receiver2) public payable {
     // One transfer call needs 2300 gas
     require(msg.sender.balance + 4600 > msg.value, 'A sender should have enough balance');
-    require(receivers.length == 2, 'The number of receivers should be 2');
     require(msg.value % 2 == 0, 'The value should be evenly divisible');
     require(msg.value > 0, 'A given value should be bigger than 0');
     require(msg.sender != receivers[0] && msg.sender != receivers[1], 'A sender should not be one of receivers');
 
     uint value = msg.value / 2;
-    address(uint160(receivers[0])).transfer(value);
-    address(uint160(receivers[1])).transfer(value);
+    receiver1.transfer(value)
+    receiver2.transfer(value)
 
     emit Transfer(msg.sender, true, receivers[0].balance, receivers[1].balance);
   }
