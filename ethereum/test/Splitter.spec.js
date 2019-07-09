138a3eab2e240070079d0b0a0a88542f8011d338
Daenam Kim <daenam.kim@gmail.com>
2019-07-09 12:14:56 +0900

    Add more test cases and refactor tests

--- ethereum/test/Splitter.spec.js
+++ ethereum/test/Splitter.spec.js
@@ -11,6 +11,10 @@ contract('Splitter', accounts => {
   const bob = accounts[2];
   const carol = accounts[3];
 
+  const toEther = value => {
+    return web3.utils.fromWei(value, 'ether');
+  };
+
   let splitterInstance;
   beforeEach(async () => {
     splitterInstance = await Splitter.deployed();
@@ -20,7 +24,7 @@ contract('Splitter', accounts => {
   });
   it('should not send ether if the number of receivers are not 2', async () => {
     try {
-      await splitterInstance.splitEther([bob], {
+      await splitterInstance.sendEther([bob], {
         value: 100,
         from: alice
       });
@@ -31,48 +35,44 @@ contract('Splitter', accounts => {
   });
   it('should not send ether if msg.value is not evenly divisible', async () => {
     try {
-      await splitterInstance.splitEther([bob], {
+      await splitterInstance.sendEther([bob, carol], {
         value: 33,
         from: alice
       });
       assert.fail();
-      A;
     } catch (error) {
       assert.equal(error.reason, 'The value should be evenly divisible');
     }
   });
-  it('should not send ether if msg.value and split value are smaller than 1', async () => {
+  it('should not send ether if divided msg.value is smaller than 1', async () => {
     try {
-      await splitterInstance.splitEther({
+      await splitterInstance.sendEther([bob, carol], {
         value: 0,
-        from: users[0].account
+        from: alice
       });
       assert.fail();
     } catch (error) {
-      assert.equal(error.reason, 'A given value should be bigger than 0 (wei)');
-    }
-
-    try {
-      await splitterInstance.splitEther({
-        value: 1, // to make (value / (users.length - 1)) fails
-        from: users[0].account
-      });
-      assert.fail();
-    } catch (error) {
-      assert.equal(
-        error.reason,
-        'A given value is too small to be sent to others'
-      );
+      assert.equal(error.reason, 'A given value should be bigger than 0');
     }
   });
-  it('should send split ether to others', async () => {
-    const value = web3.utils.toWei(1, 'ether');
-    await splitterInstance.splitEther([bob, carol], {
+  it('should not send ether if a sender is one of receivers', async () => {
+    try {
+      await splitterInstance.sendEther([alice, carol], {
+        value: 100,
+        from: alice
+      });
+      assert.fail();
+    } catch (error) {
+      assert.equal(error.reason, 'A sender should not be one of receivers');
+    }
+  });
+  it('should send divided ether to others', async () => {
+    await splitterInstance.sendEther([bob, carol], {
       from: alice,
-      value: value
+      value: web3.utils.toWei('10', 'ether')
     });
-    console.log(await web3.eth.getBalance(alice));
-    console.log(await web3.eth.getBalance(bob));
-    console.log(await web3.eth.getBalance(carol));
+
+    assert.equal(toEther(await web3.eth.getBalance(bob)), '105');
+    assert.equal(toEther(await web3.eth.getBalance(carol)), '105');
   });
 });
