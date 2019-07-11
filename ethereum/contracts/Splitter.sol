164d1567bfd57756c12c4f5e5d98d46d9fd4598b
Daenam Kim <daenam.kim@gmail.com>
2019-07-11 11:39:47 +0900

    Add withdraw

--- ethereum/contracts/Splitter.sol
+++ ethereum/contracts/Splitter.sol
@@ -1,42 +1,38 @@
 pragma solidity >=0.5.8;
 
 contract Splitter {
-  mapping(address => bool) blackAccounts;
+  mapping(address => uint) accounts;
   address owner;
 
   constructor() public {
     owner = msg.sender;
   }
 
-  event Transfer(address from, address receiver1, address receiver2);
+  event SplitHandle(address from, address receiver1, address receiver2);
 
-  function sendEther(address payable receiver1, address payable receiver2) public payable {
+  function split(address payable receiver1, address payable receiver2) public payable {
     // One transfer call needs 2300 gas
     require(msg.sender.balance + 4600 > msg.value, 'A sender should have enough balance');
     require(msg.value > 0, 'A given value should be bigger than 0');
     require(msg.sender != receiver1 && msg.sender != receiver2, 'A sender should not be one of receivers');
 
-    if (blackAccounts[receiver1] || blackAccounts[receiver2]) {
-      revert('A receiver is in black list');
-    }
-
     uint value = msg.value / 2;
+    accounts[receiver1] = value;
+    accounts[receiver2] = value;
 
-    // TODO: security/no-send: Consider using 'transfer' in place of 'send'.
-    if (!receiver1.send(value)) {
-      blackAccounts[receiver1] = true;
-      revert('Transaction with receiver 1 has been failed');
-    }
-
-    if (!receiver2.send(value)) {
-      blackAccounts[receiver2] = true;
-      revert('Transaction with receiver 2 has been failed');
-    }
-
-    emit Transfer(msg.sender, receiver1, receiver2);
+    emit SplitHandle(msg.sender, receiver1, receiver2);
   }
 
-  // for test only
+  event WithdrawHandle(address requester);
+
+  function withdraw() public {
+    require(accounts[msg.sender] > 0, 'A requested account should have balance');
+
+    msg.sender.transfer(accounts[msg.sender]);
+
+    emit WithdrawHandle(msg.sender);
+  }
+
   function getContractBalance() public view returns (uint) {
     return address(this).balance;
   }
