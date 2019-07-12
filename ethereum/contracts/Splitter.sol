7f228ab5dba302a6f9645f542835dc495bf8d1a7
Daenam Kim <daenam.kim@gmail.com>
2019-07-12 09:54:30 +0900

    Move all events at the top

--- ethereum/contracts/Splitter.sol
+++ ethereum/contracts/Splitter.sol
@@ -4,11 +4,12 @@ contract Splitter {
   mapping(address => uint) accounts;
   address owner;
 
+  event SplitHandle(address from, address receiver1, address receiver2);
+  event WithdrawHandle(address requester);
+
   constructor() public {
     owner = msg.sender;
   }
-
-  event SplitHandle(address from, address receiver1, address receiver2);
 
   function split(address payable receiver1, address payable receiver2) public payable {
     // One transfer call needs 2300 gas
@@ -22,8 +23,6 @@ contract Splitter {
 
     emit SplitHandle(msg.sender, receiver1, receiver2);
   }
-
-  event WithdrawHandle(address requester);
 
   function withdraw() public {
     require(accounts[msg.sender] > 0, 'A requested account should have balance');
